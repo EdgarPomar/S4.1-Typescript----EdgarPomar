1cea6b7e576937d24a208726adb26a36c671f787
edgar muñoz manjón <edgarmunozmanjon@gmail.com>
2025-04-14 09:31:08 +0200

    try to do exercise 3

--- dist/main.js
+++ dist/main.js
@@ -11,36 +11,13 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 const setupEl = document.getElementById("setup");
 const punchlineEl = document.getElementById("punchline");
 const button = document.getElementById("seguent");
-const valoracioBtns = document.querySelectorAll('[data-score]');
-const reportAcudits = [];
-let acuditActual = null;
-let puntuacioActual = null;
 const carregarAcudit = () => __awaiter(void 0, void 0, void 0, function* () {
-    if (acuditActual) {
-        const existent = reportAcudits.find(a => a.id === acuditActual.id);
-        if (existent) {
-            if (puntuacioActual !== null)
-                existent.score = puntuacioActual;
-        }
-        else {
-            reportAcudits.push({
-                id: acuditActual.id,
-                joke: `${acuditActual.setup} ${acuditActual.punchline}`,
-                score: puntuacioActual,
-                date: new Date().toISOString()
-            });
-        }
-        console.log("reportAcudits:", reportAcudits);
-    }
     try {
         const resposta = yield fetch("https://official-joke-api.appspot.com/jokes/random");
         const dades = yield resposta.json();
         console.log("Acudit rebut:", dades);
-        acuditActual = dades;
-        puntuacioActual = null;
         setupEl.textContent = dades.setup;
         punchlineEl.textContent = dades.punchline;
-        valoracioBtns.forEach(btn => btn.classList.remove("selected"));
     }
     catch (error) {
         setupEl.textContent = "No s'ha pogut carregar l'acudit.";
@@ -48,13 +25,6 @@ const carregarAcudit = () => __awaiter(void 0, void 0, void 0, function* () {
         console.error("Error carregant acudit:", error);
     }
 });
-valoracioBtns.forEach(button => {
-    button.addEventListener("click", () => {
-        puntuacioActual = Number(button.dataset.score);
-        valoracioBtns.forEach(btn => btn.classList.remove("selected"));
-        button.classList.add("selected");
-    });
-});
 button.addEventListener("click", carregarAcudit);
-// Carrega el primer acudit
+// Carreguem un acudit en iniciar
 carregarAcudit();
